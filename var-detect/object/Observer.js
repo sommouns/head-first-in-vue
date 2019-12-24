81fbbb696ae0785f131348a7498d59ab24b5913b
sommouns <969728883@qq.com>
2019-12-24 22:00:52 +0800

    [feature][对象数据侦测]添加Observer类

---
+++ var-detect/object/Observer.js
@@ -0,0 +1,48 @@
+/**
+ * @file Observer类用于递归监听所有key
+ */
+
+import Dep from './Dep';
+
+export default class Observer {
+    constructor(value) {
+        this.value = value;
+
+        if (!Array.isArray(value)) {
+            this.walk(value);
+        }
+    }
+
+    walk (obj) {
+        const keys = Object.keys(obj);
+        keys.forEach(v => {
+            defineReactive(obj, v, obj[v]);
+        });
+    }
+}
+
+function defineReactive (data, key, val) {
+
+    // 递归转换子对象
+    if (typeof val === 'object') {
+        new Observer(val);
+    }
+
+    let dep = new Dep();
+    Object.defineProperty(data, key, {
+        enumerable: true,
+        configurable: true,
+        get () {
+            dep.depend();
+            return val;
+        },
+        set (newVal) {
+            if (val === newVal) {
+                return;
+            }
+
+            val = newVal;
+            dep.notify();
+        }
+    });
+}
